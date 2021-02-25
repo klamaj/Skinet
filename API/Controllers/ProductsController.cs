c4dec917e0c5635087281ba3d2f99915899c0474
Kristel Lamaj <kri.lamaj@gmail.com>
2021-02-25 22:41:27 +0200

    Eager loading of navigation properties

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -36,5 +36,17 @@ namespace API.Controllers
             // return await _context.Products.FindAsync(id);
             return await _repo.GetProductByIdAsync(id);
         }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
     }
 }
